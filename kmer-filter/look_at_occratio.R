08786920f8fad7f0d5ff0b44a2438ec4b436aaa1
Dan Bolser <dbolser@ebi.ac.uk>
2018-02-26 16:46:41 +0000

    tidying the R script a bit

--- kmer-filter/look_at_occratio.R
+++ kmer-filter/look_at_occratio.R
@@ -1,44 +1,130 @@
 
+my.dat <-
+  read.table("Data/161010_Chinese_Spring_v1.0_pseudomolecules.occratio.10.45.fix")
 
 my.dat <-
-  read.table("Data/PGSC0003DMS.occratio.10.45.fix")
+  read.table("Data/Hordeum_vulgare.Hv_IBSC_PGSB_v2.dna.toplevel.occratio.10.45.fix")
 
-my.dat <-
-  read.table("Data/PGSC0003DMB.occratio.10.45.fix")
-
-head(my.dat)
+head(my.dat, 6)
 nrow(my.dat)
 
 names(my.dat) <-
   c("k",
-    "uniq.mer.n",    "uniq.mer.f",
-    "rept.mer.uc.n", "rept.mer.uc.f",
-    "rept.mer.dc.n", "rept.mer.dc.f",
-    "n.mer.uc",
-    "n.mer.al")
+    
+    ## unique ....... # distribution of unique mers
+    "uniq.n",
+    "u.f",
+    
+    ## nonunique .... # distribution of non unique mers (counting each
+    ##                  non unique mer only once)
+    "rept.n",
+    "r.f",
+    
+    ## nonuniquemulti # distribution of non unique mers (counting each
+    ##                  non unique mer more than once)
+    "rept.mn",
+    "r.mf",
+    
+    ## total ........ # distribution of all mers (counting each non
+    ##                  unique mer only once)
+    "all.n",
+
+    ## relative ..... # distribution of all mers (counting each non
+    ##                  unique mer more than once)
+    "all.mn"
+    )
+
+head(my.dat, 10)
 
 
-plot(uniq.mer.f ~ k, data = my.dat,
+
+## Plot total number of distinct kmers with k
+
+## This plot includes kmers that occur once or multiple times, but
+## counts each unique mer only once regardless
+plot(all.n ~ k, data = my.dat,
+     main="Number of distinct kmers, unique or not, against k")
+
+## General observations, as k grows, there is a rapid growth in number
+## of kmers. There is a fast growth in the number of kmers at a
+## certain critical size.
+
+## Is this the kmer size after which growth is 'stable'?
+abline(v=21)
+
+
+
+## Plot total number of uniqe kmers with k
+
+## This plot only includes kmers that occur exactly once
+plot(uniq.n ~ k, data = my.dat,
+     main="Number of distinct unique kmers against k")
+
+abline(v=21)
+
+## General observations, at low values of k, all kmers are repetative
+## (occur more than once).
+
+
+
+## Both
+
+matplot(x=my.dat$k, y=my.dat[,c('all.n', 'uniq.n')],
+        type='b', pch=c(1,2))
+
+abline(v=21)
+
+
+## Ratio 
+plot(uniq.n / all.n ~ k, data = my.dat)
+
+abline(v=21)
+
+abline(v=16, col=2)
+abline(v=17, col=3)
+abline(v=18, col=4)
+abline(v=19, col=5)
+abline(v=20, col=6)
+abline(v=21, col=7)
+
+plot(rept.n / all.n ~ k, data = my.dat,
+     main="Fraction of distinct unique kmers against distinct unique kmers against k")
+
+
+
+
+
+
+
+
+
+
+## Plot fraction of total number of uniqe kmers with k
+plot(u.f ~ k, data = my.dat,
      ylim=c(0,1), main="Fraction of kmers unique with k")
 ##
 abline(v=19, col=2)
 abline(v=41, col=2)
-abline(h=0.86, col=2)
-abline(h=0.95, col=2)
+abline(h=0.80, col=5)
+abline(h=0.86, col=2, lwd=3)
 
-
-plot(rept.mer.uc.f ~ k, data = my.dat, type="b",
+plot(r.f ~ k, data = my.dat, type="b",
      ylim=c(0,1))
-points(uniq.mer.f ~ k, data = my.dat, type="b")
+points(u.f ~ k, data = my.dat, type="b")
 ##
 abline(v=19, col=2)
 abline(v=41, col=2)
-abline(h=0.86, col=2)
-abline(h=0.95, col=2)
+abline(h=0.80, col=5)
+abline(h=0.86, col=2, lwd=3)
 
 
 
-plot(rept.mer.dc.f ~ k, data = my.dat, type="b",
+
+plot(r.mf ~ k, data = my.dat, type="b",
      ylim=c(0,1)#, main="Fraction of kmers unique with k"
+     )
+
+
+plot(rept.mn ~ k, data = my.dat, type="b"
      )
 
